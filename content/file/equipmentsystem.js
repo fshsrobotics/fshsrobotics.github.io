b7f8afcdff9dc78c29be12c71591d05ed1f1dd02
Kris <kris.choy1@gmail.com>
2018-06-24 15:27:48 +1000

    Add about, awards, attendance and equipment

--- content/file/equipmentsystem.js
+++ content/file/equipmentsystem.js
@@ -10,19 +10,156 @@ var config = {
 firebase.initializeApp(config);
 var db = firebase.database();
 
+var loginSys = {};
 var equipSys = {};
+var attendanceSys = {};
+
+attendanceSys.currRollCache = [];
+
+attendanceSys.getAllRolls = function(week, studentid) {
+    db.ref('/roll/').once('value').then(function(snapshot) {
+        console.log(snapshot.val());
+        $.each(snapshot.val(), function (i, v) {
+            $('#adminattendance-week').append($('<option>', { 
+                value: i,
+                text : i 
+            }));
+        });
+
+        $('#adminattendance-week').on('change', function(e) {
+            attendanceSys.showAttendanceInfo($(this).val());
+        });
+
+    });
+}
+
+attendanceSys.addStudentAttend = function(week, studentid) {
+    db.ref('/roll/' + week).once('value').then(function(snapshot) {
+        if (snapshot.val() !== null) {
+            var newAttendanceKey = db.ref('/roll/' + week + '/students/').push().key;
+            var updates = {};
+            updates['/roll/' + week + '/students/' + newAttendanceKey] = studentid;
+            db.ref().update(updates, attendanceSys.showAttendanceInfo(week));
+        } else {
+            db.ref('/roll/' + week).set({
+                students: [studentid]
+            });
+            attendanceSys.showAttendanceInfo(week);
+        }
+    });
+}
+
+attendanceSys.showAttendanceInfo = function(week) {
+    $('#attendance-list').empty().append("Loading attendance...");
+    db.ref('/students/').once('value').then(function(studentssnapshot) {
+        db.ref('/roll/' + week + '/').once('value').then(function(snapshot) {
+            var todayAttendance = snapshot.val();
+            var currstudents = studentssnapshot.val();
+            if (todayAttendance == null) {
+                $('#attendance-list').empty().append("No attendance today");
+                return;
+            }
+            // Load attendance list
+            $('#attendance-list').empty();
+            $('#attendance-header').empty().append('Attendance for ' + week);
+
+            var shownStudents = [];
+
+            $.each(todayAttendance.students, function(i, studentAttended) {
+                if (studentAttended && currstudents[studentAttended] && shownStudents.indexOf(studentAttended) <= -1) {
+                    shownStudents.push(studentAttended);
+                    $('#attendance-list').append('<li>' + currstudents[studentAttended].name + '</li>');
+                }
+            });
+            // Load group equipment info
+            $('#es-group-info').append("<b>Equipment borrowed</b>");
+            if (!snapshot.val().equipment) {
+                $('#es-group-info').append("<div>No records found</div>");
+                return;
+            }
+        });
+    });
+}
+
+/**
+ * Login via Firebase to enable write access
+ * @param  type the type of login as a string
+ * @param  password the password for the Firebase user
+ */
+loginSys.login = function(type, password) {
+    $('#ls-' + type + '-login-status').empty();
+    var email;
+    if (type === "student") {
+        email = "student@robo.kcnotes.net";
+    } else if (type === "admin") {
+        email = "admin@robo.kcnotes.net";
+    } else {
+        return;
+    }
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+        if (type === "student") {
+            loginSys.showStudentInterface();
+        }
+    }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        $('#ls-' + type + '-login-status').append("Error: " + error.message);
+    });
+}
+
+loginSys.logout = function() {
+    firebase.auth().signOut().then(function(d) {
+        loginSys.showLoginInterface();
+    }).catch(function(error) {
+        // error
+    });
+}
+
+loginSys.showStudentInterface = function() {
+    $('#loginsystem').hide();
+    $('#equipmentsystem').show();
+    equipSys.getMembers().then(function() {
+        $('#es-allgroups').empty();
+        $.each(equipSys.members, function(i, group) {
+            $('#es-allgroups').append('<b>' + i + '</b>: ' + group.members.join(', ') + '<br/>');
+        });
+    });
+}
+loginSys.showAdminInterface = function() {
+    $('#loginsystem').hide();
+    $('#equipmentsystem').hide();
+    $('#adminsystem').show();
+    if ($('#adminattendance').length) {
+        $('#adminattendance').show();
+        attendanceSys.getAllRolls();
+    }
+}
+loginSys.showLoginInterface = function() {
+    $('#loginsystem').show();
+    $('#equipmentsystem').hide();
+    $('#adminsystem').hide();
+    $('#adminattendance').hide();
+}
 
 equipSys.regroupid = /^[a-zA-Z]{2}\d{2}$/g;
 
-// Get a group of all members and their groups
-equipSys.getMembers = function () {
+/**
+ * Get a list of members in a group
+ * @return list of members
+ */
+equipSys.getMembers = function () { 
     return db.ref('/groups/').once('value').then(function(snapshot) {
         equipSys.members = snapshot.val();
     });
 }
 
+/**
+ * Borrow an equipment to the database for a particular group
+ * @param groupid the group's assigned ID
+ * @param equipid the equipment ID they are borrowing
+ * @param sticker (optional) the sticker on the equipment hey are borrowing
+ */
 equipSys.addEquip = function(groupid, equipid, sticker) {
-    console.log(groupid, equipid, sticker);
     db.ref('/groups/' + groupid + '/').once('value').then(function(snapshot) {
         if (snapshot.val() !== null) {
             var newEquipKey = db.ref('/groups/' + groupid + '/equipment/').push().key;
@@ -39,18 +176,21 @@ equipSys.addEquip = function(groupid, equipid, sticker) {
     });
 }
 
+/**
+ * Get the equipment name from an ID
+ * @param  equipid the equipment ID you want the name for
+ * @return the readable equipment name
+ */
 equipSys.getEquipmentName = function(equipid) {
     var name = equipSys.equipids[equipid];
     if (name) return name;
     return 'Unknown equipment';
 }
 
-// equipSys.addMember = function (user, groupID) {
-//     var updates = {};
-//     updates['/users/' + user] = groupID;
-//     return db.ref().update(updates);
-// }
-
+/**
+ * Get the request from the equipment form
+ * @return an object with groupid, equipment and stickerid
+ */
 equipSys.getResponse = function() {
     var groupid = $('#es-groupid').val().toUpperCase();
     var equipment = $('#es-equipment option:selected').attr('data-eid');
@@ -106,9 +246,59 @@ equipSys.fillGroupInformation = function(groupid) {
     });
 }
 
-equipSys.init = function() {
-    equipSys.getMembers().then(function() {
-        console.log(equipSys.members);
+function init() {
+    $('#loginsystem').hide();
+    $('#adminsystem').hide();
+    $('#adminattendance').hide();
+    $('#equipmentsystem').hide();
+    var pages = ['Equipment', 'equipment', 'Attendance', 'attendance'];
+    if (pages.indexOf(wikispaces_page) < 0) return;
+    firebase.auth().onAuthStateChanged(function(user) {
+        $('#login-loading').hide();
+        if (user) {
+            if (user.email === "student@robo.kcnotes.net") {
+                loginSys.showStudentInterface();
+            } else if (user.email === "admin@robo.kcnotes.net") {
+                loginSys.showAdminInterface();
+            }
+        } else {
+            $('#loginsystem').show();
+        }
+    });
+
+    $('#robosystem form').submit(function(e) {
+        e.preventDefault();
+    });
+    $('#ls-student-login').on('click', function() {
+        loginSys.login("student", $('#ls-student').val());
+        $('#ls-student').val("");
+    });
+    $('#ls-admin-login').on('click', function() {
+        loginSys.login("admin", $('#ls-admin').val());
+        $('#ls-admin').val("");
+    });
+    $('.ls-logout').on('click', function() {
+        loginSys.logout();
+    });
+    $('#attendance-check').on('click', function() {
+        var reStudentID = new RegExp(/^\d{9}$/i);
+        if (attendanceSys.currRollCache.indexOf($('#attendance-student').val()) > -1) {
+            $('#attendance-submit-status').empty().append("Oy please don't >:(");
+            $('#attendance-student').val('');
+            $('#attendance-student').focus();
+            return;
+        } else if (!reStudentID.test($('#attendance-student').val())) {
+            $('#attendance-submit-status').empty().append("Oy that's not a valid student ID >:(");
+            $('#attendance-student').val('');
+            $('#attendance-student').focus();
+            return;
+        } else {
+            $('#attendance-submit-status').empty();
+            attendanceSys.currRollCache.push($('#attendance-student').val());
+        }
+        attendanceSys.addStudentAttend($('#attendance-week').val(), $('#attendance-student').val());
+        $('#attendance-student').val('');
+        $('#attendance-student').focus();
     });
     $('#es-groupid').on('input', function(e){
         equipSys.loadGroupData();
@@ -146,5 +336,5 @@ equipSys.equipids = {
     pixy: "Pixy Camera"
 }
 $(function() {
-    equipSys.init();
+    init();
 });
